4d0a2d73706fd1255b2ba8d52336f2a484462548
joaovitors1g <joao_vitor9846@hotmail.com>
2022-10-12 18:06:42 -0300

    feat: add layout styles

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,10 +1,11 @@
 import { Header } from '../../components/Header'
 import { Outlet } from 'react-router-dom'
+import { LayoutContainer } from './styles'
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
