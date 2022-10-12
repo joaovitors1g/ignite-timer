31a98bca813d3169edd429026d5c94f79ee5702b
joaovitors1g <joao_vitor9846@hotmail.com>
2022-10-12 18:25:16 -0300

    feat: add route links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,19 @@ import { HeaderContainer } from './styles'
 
 import logo from '../../assets/logo.svg'
 import { Timer, Scroll } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <img src={logo} alt="" />
       <nav>
-        <a href="#">
+        <NavLink to="/" title="Temporizador">
           <Timer size={24} />
-        </a>
-        <a href="#">
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
           <Scroll size={24} />
-        </a>
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
