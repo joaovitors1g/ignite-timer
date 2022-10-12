9efa3b7ed88dc943578ae97d39460aa768567328
joaovitors1g <joao_vitor9846@hotmail.com>
2022-10-12 20:45:51 -0300

    feat: add form and countdown styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,36 @@
 import { Play } from 'phosphor-react'
+import {
+  Colon,
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+} from './styles'
 
 export function Home() {
   return (
-    <div>
+    <HomeContainer>
       <form>
-        <label htmlFor="task">Vou trabalhar em</label>
-        <input id="task" />
-        <label htmlFor="minutes-amount">Durante</label>
-        <input type="number" id="minutes-amount" />
-        <span>minutos.</span>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+          <label htmlFor="minutes-amount">durante</label>
+          <input type="number" id="minutes-amount" />
+          <span>minutos.</span>
+        </FormContainer>
 
-        <div>
+        <CountdownContainer>
           <span>0</span>
           <span>0</span>
-          <span>:</span>
+          <Colon>:</Colon>
           <span>0</span>
           <span>0</span>
-        </div>
+        </CountdownContainer>
 
         <button type="submit">
           <Play size={24} />
           Começar
         </button>
       </form>
-    </div>
+    </HomeContainer>
   )
 }
