1b97ccb7d492f04b28766a76e835e03e6949fc6e
joaovitors1g <joao_vitor9846@hotmail.com>
2022-10-16 14:07:07 -0300

    refactor: move new cycle form into new component

---
+++ src/components/NewCycleForm/index.tsx
@@ -0,0 +1,36 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+
+export function NewCycleForm() {
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        placeholder="Dê um nome para o seu projeto"
+        list="task-suggestions"
+        id="task"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+      <datalist id="task-suggestions">
+        <option value="Projeto 1"></option>
+        <option value="Projeto 2"></option>
+        <option value="Projeto 3"></option>
+        <option value="Projeto 4"></option>
+      </datalist>
+      <label htmlFor="minutes-amount">durante</label>
+      <MinutesAmountInput
+        type="number"
+        placeholder="00"
+        id="minutes-amount"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', {
+          valueAsNumber: true,
+        })}
+      />
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -133,37 +133,6 @@ export function Home() {
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateCycle)}>
-        <FormContainer>
-          <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput
-            placeholder="Dê um nome para o seu projeto"
-            list="task-suggestions"
-            id="task"
-            disabled={!!activeCycle}
-            {...register('task')}
-          />
-          <datalist id="task-suggestions">
-            <option value="Projeto 1"></option>
-            <option value="Projeto 2"></option>
-            <option value="Projeto 3"></option>
-            <option value="Projeto 4"></option>
-          </datalist>
-          <label htmlFor="minutes-amount">durante</label>
-          <MinutesAmountInput
-            type="number"
-            placeholder="00"
-            id="minutes-amount"
-            step={5}
-            min={5}
-            max={60}
-            disabled={!!activeCycle}
-            {...register('minutesAmount', {
-              valueAsNumber: true,
-            })}
-          />
-          <span>minutos.</span>
-        </FormContainer>
-
         <CountdownContainer>
           <span>{minutes[0]}</span>
           <span>{minutes[1]}</span>
